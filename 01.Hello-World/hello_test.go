f6fb66190a4bce6ef775c0f091212e73e03803f9
Tino <Konstantin.v.milchev@gmail.com>
2023-11-10 22:19:54 +0000

    Added default value

--- 01.Hello-World/hello_test.go
+++ 01.Hello-World/hello_test.go
@@ -3,10 +3,24 @@ package main
 import "testing"
 
 func TestHello(t *testing.T){
+
+	t.Run("say hello to people", func(t *testing.T){
+
 	got := Hello("Chris")
 	want := "Hello, Chris"
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
+})
+
+t.Run("Default to world when no person", func(t *testing.T){
+
+	got := Hello("")
+	want := "Hello, world"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+})
 }
