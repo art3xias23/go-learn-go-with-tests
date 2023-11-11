20f70ddff807ee8f663ca975f9a1f0686f4cba23
Tino <Konstantin.v.milchev@gmail.com>
2023-11-11 17:06:25 +0000

    Replaced arrays with slices

--- 04.Arrays_Slices/arrays.go
+++ 04.Arrays_Slices/arrays.go
@@ -1,8 +1,8 @@
 package arrays
 
-func Sum(numbers [5]int) (sumation int) {
-	for _, number:=range numbers {
-		sumation +=number 
+func Sum(numbers []int) (sumation int) {
+	for _, number := range numbers {
+		sumation += number
 	}
 	return
 }
--- 04.Arrays_Slices/arrays_test.go
+++ 04.Arrays_Slices/arrays_test.go
@@ -1,14 +1,26 @@
 package arrays
-
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{4, 5, 6, 7}
+	t.Run("Testing a sum of array with 5 elements", func(t *testing.T) {
+		numbers := []int{4, 5, 6, 7}
 
-	got := Sum(numbers)
-	expected := 22
+		got := Sum(numbers)
+		expected := 22
 
-	if expected != got {
-		t.Errorf("expected %d, got %d", expected, got)
-	}
+		if expected != got {
+			t.Errorf("expected %d, got %d", expected, got)
+		}
+	})
+
+	t.Run("Testing a sum of array with any size of elements", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := Sum(numbers)
+		expected := 6
+
+		if expected != got {
+			t.Errorf("expected %d, got %d", expected, got)
+		}
+	})
 }
