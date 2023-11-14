2cb977f028eaa11aea8bbdc1d817e10c42a48fd7
Tino <Konstantin.v.milchev@gmail.com>
2023-11-14 10:49:00 +0000

    Abstracting both write and sleep to the same struct

--- 09.Mocking/mocking.go
+++ 09.Mocking/mocking.go
@@ -4,17 +4,17 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 const goWording = "Go!"
 
 func main(){
-	Countdown(os.Stdout, 3, &SpySleeper{})
+	Countdown(os.Stdout, 3, &DefaultSleeper{})
 }
 func Countdown(writer io.Writer, count int, slp Sleeper){
 	for cnt :=count; cnt > 0; cnt--{
 	fmt.Fprintln(writer, cnt)
 	slp.Sleep()
-	//time.Sleep(1*time.Second)
 	}
 
 	fmt.Fprint(writer, goWording)
@@ -28,6 +28,28 @@ type SpySleeper struct{
 	Calls int
 }
 
+type DefaultSleeper struct{}
+
+func(d *DefaultSleeper) Sleep(){
+	time.Sleep(1*time.Second)
+}
+
 func(s *SpySleeper) Sleep(){
 	s.Calls++
 }
+
+type SpyCountdownOperations struct{
+	Calls []string
+}
+
+func(s *SpyCountdownOperations) Write([]byte)(n int, err error){
+	s.Calls=append(s.Calls, write)
+	return
+}
+
+func(s *SpyCountdownOperations) Sleep(){
+	s.Calls=append(s.Calls, sleep)
+}
+
+const sleep = "sleep"
+const write = "write"
--- 09.Mocking/mocking_test.go
+++ 09.Mocking/mocking_test.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
 )
 
 func Test_Countdown(t *testing.T){
+	t.Run("Test normal output", func(t *testing.T){
+
 	buffer:= bytes.Buffer{}
 spySleep:= SpySleeper{}
 cnt:=3 
@@ -24,4 +27,24 @@ Go!`
 	if cnt!=spySleep.Calls{
 		t.Errorf("SpySleep: got %d, wanted %d", spySleep.Calls, cnt)
 	}
+	})
+
+	t.Run("Test sequence output", func(t *testing.T){
+		want:=[]string{write,
+					sleep,
+					write,
+					sleep,
+					write,
+					sleep,
+					write,
+}
+spySleep:=SpyCountdownOperations{}
+Countdown(&spySleep, 3, &spySleep)
+got:= spySleep.Calls
+
+if !reflect.DeepEqual(want, got){
+	t.Errorf("got %v, want %v", got, want)
+}
+	})
+
 }
