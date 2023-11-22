cc9cae66dec69d9c19705ee5cc4c18dbe7913bfe
Tino <Konstantin.v.milchev@gmail.com>
2023-11-22 21:32:37 +0000

    WIP Maths

--- 13.Maths/maths.go
+++ 13.Maths/maths.go
@@ -11,5 +11,6 @@ type Point struct {
 // SecondHand is the unit vector of the second hand of an analogue clock at time `t`
 // represented as a Point.
 func SecondHand(t time.Time) Point {
-	return Point{}
+	return Point{150, 60}
+	return 
 }
--- 13.Maths/maths_test.go
+++ 13.Maths/maths_test.go
@@ -1,7 +1,6 @@
-package clockface_test
+package clockface
 
 import (
-	"playground/clockface"
 	"testing"
 	"time"
 )
@@ -9,8 +8,8 @@ import (
 func TestSecondHandAtMidnight(t *testing.T) {
 	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
 
-	want := clockface.Point{X: 150, Y: 150 - 90}
-	got := clockface.SecondHand(tm)
+	want := Point{X: 150, Y: 150 - 90}
+	got := SecondHand(tm)
 
 	if got != want {
 		t.Errorf("Got %v, wanted %v", got, want)
